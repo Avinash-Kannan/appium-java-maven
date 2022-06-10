794466d7949379fce2d6e889f8896e05d92177da
avinashk <avinashk.mech@gmail.com>
2022-06-10 13:26:25 +0530

    Updated Android workflow

--- src/test/java/BasicConfig.java
+++ src/test/java/BasicConfig.java
@@ -1,3 +1,5 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -9,34 +11,35 @@ import java.net.URL;
 
 public class BasicConfig {
 
-    static IOSDriver<IOSElement> driver;
+    //static IOSDriver<IOSElement> driver;
+    static AndroidDriver<AndroidElement> driver;
 
     @BeforeTest
     public void setUpCapabilities() throws MalformedURLException {
         DesiredCapabilities capabilities= new DesiredCapabilities();
 
         // Android capabilities
-//        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
-//        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"ANDROID");
-//       // caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"11");
-//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"TestEmulator");
-//        capabilities.setCapability(MobileCapabilityType.APP,"/Users/avinashkannan/Documents/gatling-workspace/appium-java-demo/src/test/app/ApiDemos.apk");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,"ANDROID");
+       // caps.setCapability(MobileCapabilityType.PLATFORM_VERSION,"11");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"TestEmulator");
+        capabilities.setCapability(MobileCapabilityType.APP,"src/test/app/ApiDemos.apk");
 
 //        // iOS capabilities
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.4");
+//        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
+//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11");
+//        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
+//        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.4");
         // MobileCapabilityType.APP is set to the IOS app bundle file path that was created in Step 1. Replace it with your app bundle file path.
-        capabilities
-                .setCapability(MobileCapabilityType.APP,
-                        "src/test/app/TestApp.app");
+//        capabilities
+//                .setCapability(MobileCapabilityType.APP,
+//                        "src/test/app/TestApp.app");
 
 
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
-        driver = new IOSDriver<IOSElement>(url, capabilities);
-        //driver = new AndroidDriver<AndroidElement>(url, capabilities);
+        //driver = new IOSDriver<IOSElement>(url, capabilities);
+        driver = new AndroidDriver<AndroidElement>(url, capabilities);
         WebDriverWait wait = new WebDriverWait(driver, 10);
     }
 
--- src/test/java/SampleTest.java
+++ src/test/java/SampleTest.java
@@ -1,3 +1,4 @@
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.ios.IOSElement;
 import org.testng.annotations.Test;
 import java.util.List;
@@ -9,16 +10,16 @@ public class SampleTest extends BasicConfig {
     @Test
     public static void TestApp() {
         // Android
-//        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
-//		driver.findElementByXPath("//android.widget.TextView[@text='1. Preferences from XML']").click();
-//        List<IOSElement> elementList = driver.findElementsByClassName("android.widget.CheckBox");
-//		elementList.get(1).click();
-//		elementList.get(2).click();
+        driver.findElementByXPath("//android.widget.TextView[@text='Preference']").click();
+		driver.findElementByXPath("//android.widget.TextView[@text='1. Preferences from XML']").click();
+        List<AndroidElement> elementList = driver.findElementsByClassName("android.widget.CheckBox");
+		elementList.get(1).click();
+		elementList.get(2).click();
 
         // iOS
-        driver.findElementByXPath("//XCUIElementTypeTextField[@name='IntegerA']").sendKeys("3");
-        driver.findElementByXPath("//XCUIElementTypeTextField[@name='IntegerB']").sendKeys("5");
-        driver.findElementByXPath("//XCUIElementTypeStaticText[@name='Compute Sum']").click();
+//        driver.findElementByXPath("//XCUIElementTypeTextField[@name='IntegerA']").sendKeys("3");
+//        driver.findElementByXPath("//XCUIElementTypeTextField[@name='IntegerB']").sendKeys("5");
+//        driver.findElementByXPath("//XCUIElementTypeStaticText[@name='Compute Sum']").click();
 
     }
 
