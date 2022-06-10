82ac0ef6ee2aa09812f4531a64aedd8d03207c41
avinashk <avinashk.mech@gmail.com>
2022-06-10 12:53:57 +0530

    Updated iOS workflow

--- src/test/java/BasicConfig.java
+++ src/test/java/BasicConfig.java
@@ -24,16 +24,16 @@ public class BasicConfig {
 
 //        // iOS capabilities
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 13");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "iPhone 11");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "iOS");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "15.0");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "14.4");
         // MobileCapabilityType.APP is set to the IOS app bundle file path that was created in Step 1. Replace it with your app bundle file path.
         capabilities
                 .setCapability(MobileCapabilityType.APP,
                         "src/test/app/TestApp.app");
 
 
-        URL url = new URL("http://127.0.0.1:8100/wd/hub");
+        URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
         driver = new IOSDriver<IOSElement>(url, capabilities);
         //driver = new AndroidDriver<AndroidElement>(url, capabilities);
